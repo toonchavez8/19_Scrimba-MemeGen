d78e99504f75d38f18e1852b51d6642b31a48b19
Miguel Chavez <toonchavez8@gmail.com>
2023-01-03 22:27:41 -0600

    create api folder and meme data js | add func to meme.jsx to randomably sort through meme data and get url

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import "../CSS/Meme.css"
+import memesData from '../api/memesData'
 
 export default function Meme() {
+
+    const [memeUrl, setMemeUrl] = React.useState()
+
+    function getMemeImg(){
+        const memes = memesData.data.memes
+        
+        const randomMeme = memes[Math.floor(Math.random() * memes.length)]
+      
+        const url = randomMeme.url
+
+        setMemeUrl(url)
+        console.log(url)
+    }
+
     return (
         <main>
-            <form className='form' >
+            <div className='form' >
                 <input className='form--input' type="text" placeholder="Top Text" />
                 <input className='form--input' type="text" placeholder="Bottom Text" />
-                <button className='form--button' >Get a Meme 
+                <button className='form--button' 
+                onClick={getMemeImg}
+                >Get a Meme 
                     <span className='form--reaction'  role="img" aria-label="meme">🤣</span>
                 </button>
-            </form>
+            </div>
+            <figure className='memeContainer'>
+                <img src={memeUrl} alt/>
+            </figure>
+
         </main>
     )
 }
